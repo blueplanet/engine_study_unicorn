aee8d9fb96d462fdb99f8531b4b14eb836040d72
blueplanet <erguolinge@gmail.com>
2016-10-16 07:33:06 +0900

    Add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015222831) do
+ActiveRecord::Schema.define(version: 20161015223211) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20161015222831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_blorgh_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
